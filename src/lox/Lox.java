543b73357eecf86cab06d48e86a319e05d54d157
Microcline <Microcline@users.noreply.github.com>
2025-08-14 16:45:50 -0500

    Ch. 4 - Scanning
    Tokens, lexical analysis, scanner class addition

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -7,8 +7,10 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+//import java.util.Scanner;
 
 public class Lox {
+	static boolean hadError = false;
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
 			System.out.println("Usage: jlox [script]");
@@ -22,14 +24,46 @@ public class Lox {
 		}
 	}
 
-	private static void runPrompt() {
-		// TODO Auto-generated method stub
+	private static void runPrompt() throws IOException {
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+		
+		for(;;) {
+			System.out.print("> ");
+			String line = reader.readLine();
+			if(line == null) {
+				break;
+			}
+			run(line);
+		}
 		
 	}
 
-	private static void runFile(String string) {
-		// TODO Auto-generated method stub
+	private static void run(String source) {
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+		
+		for(Token token : tokens) {
+			System.out.println(token);
+		}
 		
 	}
+	
+	static void error(int line, String message) {
+		report(line, "", message);
+	}
+	
+	private static void report(int line, String where, String message) {
+		System.err.println("[line " + line + "] Error" + where + ":" + message);
+		hadError = true;
+	}
 
+	private static void runFile(String path) throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
+		
+		if(hadError) {
+			System.exit(65);
+		}
+	}
 }
